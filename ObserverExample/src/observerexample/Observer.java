ad3617bf8bcc69d7f9480973f7a74c2890e7a376
Giorgos Valamatsas <giorgos.valamats@gmail.com>
2019-09-07 13:27:36 +0300

    Factory Method

---
+++ ObserverExample/src/observerexample/Observer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observerexample;
+
+/**
+ *
+ * @author giorgos
+ */
+public interface Customer {
+    public void update(Product product);
+}
+
+
+/*
+The Observer interface:
+
+- contains only the update() method declaration
+
+
+*/
