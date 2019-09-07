ea3de8fa1d1e1249d793baf8aeeaff35670bc632
Giorgos Valamatsas <giorgos.valamats@gmail.com>
2019-09-07 20:38:07 +0300

    Composite finished

--- Composite/src/composite/Composite.java
+++ Composite/src/composite/Composite.java
@@ -20,11 +20,11 @@ public class Composite {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Region country = new Region("Greece");
 
         Region department = new Region("Attica");
-        
+
         Region region = new Region("Athens A");
         department.addChild(region);
         region = new Region("Athens B");
@@ -35,9 +35,9 @@ public class Composite {
         department.addChild(region);
         region = new Region("Attica");
         department.addChild(region);
-        
+
         country.addChild(department);
-        
+
         department = new Region("Central Greece");
         region = new Region("Boeotia");
         department.addChild(region);
@@ -49,9 +49,9 @@ public class Composite {
         department.addChild(region);
         region = new Region("Phthiotis");
         department.addChild(region);
-        
+
         country.addChild(department);
-        
+
         department = new Region("Central Macedonia");
         region = new Region("Edessa");
         department.addChild(region);
@@ -63,9 +63,9 @@ public class Composite {
         department.addChild(region);
         region = new Region("Naousa");
         department.addChild(region);
-        
+
         country.addChild(department);
-        
+
         department = new Region("Crete");
         region = new Region("Heraklion");
         department.addChild(region);
@@ -75,27 +75,41 @@ public class Composite {
         department.addChild(region);
         region = new Region("Agios Nikolaos");
         department.addChild(region);
-        
+
         country.addChild(department);
-        
-        
+
         Map<Region, List<Region>> departments = country.getChildren();
         Set<Region> depKeys = departments.keySet();
         Iterator<Region> depIt = depKeys.iterator();
-        while(depIt.hasNext()) {
+        while (depIt.hasNext()) {
             Region depart = depIt.next();
-            
+
             Map<Region, List<Region>> regions = depart.getChildren();
             Set<Region> regionKeys = regions.keySet();
             Iterator<Region> regionIt = regionKeys.iterator();
-            while(regionIt.hasNext()) {
+            while (regionIt.hasNext()) {
                 System.out.println(depart.getRegionName() + " -> " + regionIt.next().getRegionName());
             }
-            
+
         }
-        
-        
-        
+
     }
-    
+
 }
+
+
+/*
+The Composite pattern is used when we need to treat a group of objects in similar
+way as a signle object. It creates a tree structure of group of objects.
+
+Composite pattern has:
+- A class that contains a group of its own objects. Here, class Region is a
+composite class as it contains a HashMap of other child objects.
+In this example, we first create a Region "country" which contains a HashMap of
+Region "departments" which in turn contain HasMaps of Region "regions".
+
+One other solution would be to create classes Department and Cities that would
+extend the Region class. In this way, these classes could have different behavior.
+
+
+*/
